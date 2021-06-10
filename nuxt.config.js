6ce564c167c18ea563c016326a3ee1f99ea0c973
Patrik <rambo3swe@gmail.com>
2021-06-10 23:22:45 +0200

    added gtm

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,11 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/google-gtag',
   ],
+  'google-gtag': {
+    id: 'G-71Z91KX62G',
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
